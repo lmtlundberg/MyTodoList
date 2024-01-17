d9fbffdf07675f5c75c8d75bbf12c099f0cacb76
Martin Lundberg <martinlundberg1999@gmail.com>
2024-01-17 15:51:36 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Martin Lundberg on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
